5b1a3878e91086d117f95e84beed3172a60e995d
shrutika19 <jaiswalshrutika19@gmail.com>
2024-05-27 16:50:46 +0530

    fetched doc from firbase

--- app/[userId]/page.jsx
+++ app/[userId]/page.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+"use client"
 
-const ProfilePage = () => {
+import React, { useEffect } from 'react'
+import app from '../Shared/firebaseConfig'
+import { doc, getDoc, getFirestore } from 'firebase/firestore'
+
+const ProfilePage = ({params}) => {
+
+  const db = getFirestore(app);
+
+  useEffect(() => {
+    console.log(params.userId.replace('%40', '@'))
+    if(params){
+      getUserInfo(params.userId.replace('%40', '@'))
+    }
+  }, [params])
+
+
+  const getUserInfo =  async(email) => {
+    const docRef = doc(db, "user", email);
+    const docSnap = await getDoc(docRef);
+
+    if(docSnap.exists()){
+      console.log("doc data", docSnap);
+    }else{
+      console.log("no such doc");
+    }
+  }
+
   return (
     <div>ProfilePage</div>
   )
