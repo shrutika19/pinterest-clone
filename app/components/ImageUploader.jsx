0939a53ad5dc656a373168902a844623a6757afc
shrutika19 <jaiswalshrutika19@gmail.com>
2024-05-29 20:37:16 +0530

    added form component

---
+++ app/components/ImageUploader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { HiArrowCircleUp } from "react-icons/hi";
+
+const ImageUploader = () => {
+  return (
+    <div className='h-[450px] bg-[#e9e9e9] rounded-lg'>
+        <label className='m-5 flex flex-col justify-center items-center cursor-pointer h-[90%]
+        border-[2px] border-gray-300 border-dashed rounded-lg text-gray-600 '>
+          <HiArrowCircleUp className='text-[22px]'/>
+          <h2 className='font-semibold'>Click to Upload</h2>
+          <input id='dropdown-file' type='file' class="hidden"/>
+        </label>
+    </div>
+  )
+}
+
+export default ImageUploader
--- app/components/PinForm.jsx
+++ app/components/PinForm.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import ImageUploader from './../components/ImageUploader'
 
 const PinForm = () => {
   return (
-    <div>Form</div>
+    <div className='bg-white p-16 rounded-2xl'>
+        <div className='flex justify-end mb-6'>
+          <button className='bg-red-500 p-2 text-white font-semibold px-3 rounded-lg'>
+            Save
+          </button>
+        </div>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-10'>
+           <ImageUploader />
+            <div className='col-span-2'> 
+              <div className='w-[100%]'>
+                  <input 
+                  type='text' 
+                  placeholder='Add your title'
+                  className='text-[35px] outline-none font-bold w-full border-b-[2px] border-gray-400 placeholder-gray-400'/>
+                  <h2 className='text-[12px] w-full text-gray-400'>The first 40 characters are</h2>
+              </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
