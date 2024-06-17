6bc0760a4e378bde94a997ec0d359b5e843a1068
shrutika19 <jaiswalshrutika19@gmail.com>
2024-06-17 16:27:23 +0530

    updated css

--- app/components/Pins/PinItems.jsx
+++ app/components/Pins/PinItems.jsx
@@ -1,29 +1,83 @@
-import Image from 'next/image'
-import React from 'react'
-import UserTag from '../UserTag'
+// import Image from 'next/image'
+// import React from 'react'
+// import UserTag from '../UserTag'
 
-const PinItems = ({pin}) => {
+// const PinItems = ({pin}) => {
 
-    const user = {
-        name: pin?.userName,
-        image: pin?.userImage
-    }
+//     const user = {
+//         name: pin?.userName,
+//         image: pin?.userImage
+//     }
+
+//   return (
+//     <div>
+//        <div className='relative before:absolute before:h-full before:w-full before:rounded-3xl before:z-10 hover:before:bg-gray-600 before:opacity-50 cursor-pointer'>
+//          <Image 
+//             src={pin.image}
+//             alt={pin.title}
+//             width={500}
+//             height={500}
+//             className='rounded-3xl cursor-pointer relative z-0'
+//          />
+//        </div>
+//        <h2 className='font-bold text-[18px] mb-1 mt-2 line-clamp-2'>{pin.title}</h2>
+//        <UserTag user={user} />
+//     </div>
+//   )
+// }
+
+// export default PinItems
+
+import Image from 'next/image';
+import React from 'react';
+import UserTag from '../UserTag';
+
+const PinItems = ({ pin }) => {
+  const user = {
+    name: pin?.userName,
+    image: pin?.userImage,
+  };
+
+  const userTagStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: '0.5rem',
+  };
+
+  const userImageStyle = {
+    width: '1.5rem', // Adjust size
+    height: '1.5rem', // Adjust size
+    borderRadius: '50%',
+    marginRight: '0.5rem',
+  };
+
+  const userNameStyle = {
+    fontSize: '0.75rem', // Adjust size
+    fontWeight: '500',
+  };
 
   return (
-    <div>
-       <div className='relative before:absolute before:h-full before:w-full before:rounded-3xl before:z-10 hover:before:bg-gray-600 before:opacity-50 cursor-pointer'>
-         <Image 
-            src={pin.image}
-            alt={pin.title}
-            width={500}
-            height={500}
-            className='rounded-3xl cursor-pointer relative z-0'
-         />
-       </div>
-       <h2 className='font-bold text-[18px] mb-1 mt-2 line-clamp-2'>{pin.title}</h2>
-       <UserTag user={user} />
+    <div className='break-inside-avoid'>
+      <div className='relative before:absolute before:h-full before:w-full before:rounded-3xl before:z-10 hover:before:bg-gray-600 before:opacity-50 cursor-pointer'>
+        <Image 
+          src={pin.image}
+          alt={pin.title}
+          width={500}
+          height={500}
+          className='rounded-3xl cursor-pointer relative z-0'
+        />
+      </div>
+      <h2 className='font-bold text-[18px] mb-1 mt-2 line-clamp-2'>{pin.title}</h2>
+      <div style={userTagStyle}>
+        <img
+          src={user.image}
+          alt={user.name}
+          style={userImageStyle}
+        />
+        <span style={userNameStyle}>{user.name}</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PinItems
+export default PinItems;
--- app/components/Pins/PinLists.jsx
+++ app/components/Pins/PinLists.jsx
@@ -1,20 +1,38 @@
 
-import React, { useEffect } from 'react'
-import PinItems from './PinItems'
+// import React, { useEffect } from 'react'
+// import PinItems from './PinItems'
 
-const PinLists = ({listOfPins}) => {
+// const PinLists = ({listOfPins}) => {
    
-console.log("listOfPins", listOfPins)
+// console.log("listOfPins", listOfPins)
+
+//   return (
+//     <div className='mt-7 px-2 md:px-5 columns-2 md:columns-3 lg:columns-4 xl:columns-5 space-y-6 mx-auto'>
+//         {listOfPins.map((item, index) => (
+//             <div key={index}>
+//                 <PinItems  pin={item}/>
+//             </div>
+//         ))}
+//     </div>
+//   )
+// }
+
+// export default PinLists
+
+
+import React from 'react';
+import PinItems from './PinItems';
+
+const PinLists = ({ listOfPins }) => {
+  console.log("listOfPins", listOfPins);
 
   return (
     <div className='mt-7 px-2 md:px-5 columns-2 md:columns-3 lg:columns-4 xl:columns-5 space-y-6 mx-auto'>
-        {listOfPins.map((item, index) => (
-            <div key={index}>
-                <PinItems  pin={item}/>
-            </div>
-        ))}
+      {listOfPins.map((item, index) => (
+        <PinItems key={index} pin={item} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default PinLists
+export default PinLists;
