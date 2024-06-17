9d3adbf2f6cfd579668fb27dd5e2b350935554b2
shrutika19 <jaiswalshrutika19@gmail.com>
2024-06-17 11:16:24 +0530

    fetched data and displayed on ui

--- app/components/Pins/PinLists.jsx
+++ app/components/Pins/PinLists.jsx
@@ -1,15 +1,16 @@
 
 import React, { useEffect } from 'react'
+import PinItems from './PinItems'
 
 const PinLists = ({listOfPins}) => {
    
 console.log("listOfPins", listOfPins)
 
   return (
-    <div>
+    <div className='mt-7 px-2 md:px-5 columns-2 md:columns-3 lg:columns-4 xl:columns-5 space-y-6 mx-auto'>
         {listOfPins.map((item, index) => (
             <div key={index}>
-                {item.title}
+                <PinItems  pin={item}/>
             </div>
         ))}
     </div>
