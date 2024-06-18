5a4dc2be21cc27aed7f8492470377356f7a70200
shrutika19 <jaiswalshrutika19@gmail.com>
2024-06-18 09:15:31 +0530

    Display all images on home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,41 @@
+"use client"
+
+import { collection, getDocs, getFirestore, query } from "firebase/firestore";
+import app from "./Shared/firebaseConfig";
+import { useEffect, useState } from "react";
+import PinLists from "./components/Pins/PinLists";
 
 export default function Home() {
+  const db = getFirestore(app);
+  const [listOfPins,setListOfPins] = useState([]);
+
+  useEffect(() => {
+    getAllPins()
+  }, [])
+
+  // const getAllPins = async() => {
+  //   setListOfPins([]);
+  //   const q = query(collection(db,'pintrest-posts'));
+  //   const querySnapshot = await getDocs(q);
+  //   querySnapshot.forEach((doc) => {
+  //     setListOfPins((listOfPins) => 
+  //       [...listOfPins, doc.data()]);
+  //   });
+  // }
+
+  const getAllPins = async () => {
+    const q = query(collection(db, 'pintrest-posts'));
+    const querySnapshot = await getDocs(q);
+    const pins = [];
+    querySnapshot.forEach((doc) => {
+      pins.push(doc.data());
+    });
+    setListOfPins(pins);
+  };
+
   return (
-    <h1>Hey</h1>
+    <>
+      <PinLists listOfPins={listOfPins}/>
+    </>
   );
 }
